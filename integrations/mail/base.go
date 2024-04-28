a1d588e8ec9b1e91c8fe01052ce06e4f6a88fa17
blessedmadukoma <blessedmadukoma@gmail.com>
2024-04-28 23:01:15 +0100

    fix: email OTP

--- integrations/mail/base.go
+++ integrations/mail/base.go
@@ -53,15 +53,13 @@ func (r *MailRequest) send() bool {
 
 	sender := os.Getenv("EMAIL_FROM")
 	subject := r.subject
-	body := fmt.Sprintf("From: %s <%s>", r.from, os.Getenv("EMAIL_FROM")) +
-		"\r\nTo: " + r.to[0] +
-		"\r\nSubject: " +
-		r.subject + "\r\n" +
-		MIME + "\r\n" +
-		r.body
+	body := r.body
 	recipient := r.to[0]
 
-	message := mg.NewMessage(sender, subject, body, recipient)
+	message := mg.NewMessage(sender, subject, "", recipient)
+
+	// Set HTML content and content-type header
+	message.SetHtml(body)
 
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
 	defer cancel()
@@ -91,52 +89,3 @@ func (r *MailRequest) SendMail(templateName string, items interface{}) error {
 	return fmt.Errorf("failed to send the email to %s", r.to)
 
 }
-
-// net/smtp send
-// func (r *MailRequest) send() bool {
-// 	apiKey := os.Getenv("MAILGUN_API_KEY")
-// 	domain := os.Getenv("MAILGUN_DOMAIN")
-
-// 	// _ = mailgun.NewMailgun(domain, apiKey)
-// 	fmt.Println("Mail credentials:", apiKey, domain)
-
-// 	body := fmt.Sprintf("From: %s <%s>", r.from, os.Getenv("EMAIL_FROM")) +
-// 		"\r\nTo: " + r.to[0] +
-// 		"\r\nSubject: " +
-// 		r.subject + "\r\n" +
-// 		MIME + "\r\n" +
-// 		r.body
-
-// 	SMTP := fmt.Sprintf("%s:%s", os.Getenv("EMAIL_HOST"), os.Getenv("EMAIL_PORT"))
-
-// 	if err := smtp.SendMail(SMTP, smtp.PlainAuth("", os.Getenv("EMAIL_USER"), os.Getenv("EMAIL_PASSWORD"), os.Getenv("EMAIL_HOST")), os.Getenv("EMAIL_FROM"), r.to, []byte(body)); err != nil {
-// 		log.Printf("Email sending failed: %s\n", err)
-// 		return false
-// 	}
-
-// 	// sender := "blessedmadukoma@gmail.com"
-// 	// password := apiKey
-// 	// recipient := "blessedmadukoma@gmail.com"
-// 	// subject := "Fancy subject!"
-// 	// body := "Hello from Mailgun Go!"
-
-// 	// // Compose the email
-// 	// emailBody := "From: " + sender + "\r\n" +
-// 	// 	"To: " + recipient + "\r\n" +
-// 	// 	"Subject: " + subject + "\r\n\r\n" +
-// 	// 	body
-
-// 	// // Set up authentication information
-// 	// auth := smtp.PlainAuth("", sender, password, "smtp.mailgun.org")
-
-// 	// // Connect to the server, authenticate, set the sender and recipient,
-// 	// // and send the email body.
-// 	// err := smtp.SendMail("smtp.mailgun.org:587", auth, sender, []string{recipient}, []byte(emailBody))
-// 	// if err != nil {
-// 	// 	log.Fatal(err)
-// 	// 	return false
-// 	// }
-
-// 	fmt.Println("Email sent successfully!")
-// 	return true
-// }
