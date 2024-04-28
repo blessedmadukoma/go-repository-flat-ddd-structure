cfaaf8001a9048c5a3bd54fd0c528f6f4abda0e9
blessedmadukoma <blessedmadukoma@gmail.com>
2024-04-28 03:28:46 +0100

    fix: registration OTP

--- go.mod
+++ go.mod
@@ -12,6 +12,7 @@ require (
 	github.com/joho/godotenv v1.5.1
 	github.com/joshuachinemezu/throttle v0.1.0
 	github.com/lib/pq v1.10.9
+	github.com/mailgun/mailgun-go/v4 v4.12.0
 	github.com/stretchr/testify v1.8.4
 	golang.org/x/crypto v0.19.0
 )
@@ -26,6 +27,7 @@ require (
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/go-chi/chi/v5 v5.0.8 // indirect
 	github.com/go-martini/martini v0.0.0-20170121215854-22fa46961aab // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
@@ -44,6 +46,7 @@ require (
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
+	github.com/pkg/errors v0.8.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/redis/go-redis/v9 v9.0.3 // indirect
 	github.com/robfig/cron/v3 v3.0.1 // indirect
--- integrations/mail/base.go
+++ integrations/mail/base.go
@@ -2,11 +2,14 @@ package mail
 
 import (
 	"bytes"
+	"context"
 	"fmt"
 	"html/template"
 	"log"
-	"net/smtp"
 	"os"
+	"time"
+
+	"github.com/mailgun/mailgun-go/v4"
 )
 
 type MailRequest struct {
@@ -41,20 +44,36 @@ func (r *MailRequest) parseTemplate(fileName string, data interface{}) error {
 	return nil
 }
 
+// Mailgun send
 func (r *MailRequest) send() bool {
+	apiKey := os.Getenv("MAILGUN_API_KEY")
+	domain := os.Getenv("MAILGUN_DOMAIN")
+
+	mg := mailgun.NewMailgun(domain, apiKey)
+
+	sender := os.Getenv("EMAIL_FROM")
+	subject := r.subject
 	body := fmt.Sprintf("From: %s <%s>", r.from, os.Getenv("EMAIL_FROM")) +
 		"\r\nTo: " + r.to[0] +
 		"\r\nSubject: " +
 		r.subject + "\r\n" +
 		MIME + "\r\n" +
 		r.body
+	recipient := r.to[0]
 
-	SMTP := fmt.Sprintf("%s:%s", os.Getenv("EMAIL_HOST"), os.Getenv("EMAIL_PORT"))
+	message := mg.NewMessage(sender, subject, body, recipient)
 
-	if err := smtp.SendMail(SMTP, smtp.PlainAuth("", os.Getenv("EMAIL_USER"), os.Getenv("EMAIL_PASSWORD"), os.Getenv("EMAIL_HOST")), os.Getenv("EMAIL_FROM"), r.to, []byte(body)); err != nil {
-		log.Printf("Email sending failed: %s\n", err)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+
+	resp, id, err := mg.Send(ctx, message)
+
+	if err != nil {
+		log.Fatal(err)
 		return false
 	}
+
+	fmt.Printf("ID: %s Resp: %s\n", id, resp)
 	return true
 }
 
@@ -72,3 +91,52 @@ func (r *MailRequest) SendMail(templateName string, items interface{}) error {
 	return fmt.Errorf("failed to send the email to %s", r.to)
 
 }
+
+// net/smtp send
+// func (r *MailRequest) send() bool {
+// 	apiKey := os.Getenv("MAILGUN_API_KEY")
+// 	domain := os.Getenv("MAILGUN_DOMAIN")
+
+// 	// _ = mailgun.NewMailgun(domain, apiKey)
+// 	fmt.Println("Mail credentials:", apiKey, domain)
+
+// 	body := fmt.Sprintf("From: %s <%s>", r.from, os.Getenv("EMAIL_FROM")) +
+// 		"\r\nTo: " + r.to[0] +
+// 		"\r\nSubject: " +
+// 		r.subject + "\r\n" +
+// 		MIME + "\r\n" +
+// 		r.body
+
+// 	SMTP := fmt.Sprintf("%s:%s", os.Getenv("EMAIL_HOST"), os.Getenv("EMAIL_PORT"))
+
+// 	if err := smtp.SendMail(SMTP, smtp.PlainAuth("", os.Getenv("EMAIL_USER"), os.Getenv("EMAIL_PASSWORD"), os.Getenv("EMAIL_HOST")), os.Getenv("EMAIL_FROM"), r.to, []byte(body)); err != nil {
+// 		log.Printf("Email sending failed: %s\n", err)
+// 		return false
+// 	}
+
+// 	// sender := "blessedmadukoma@gmail.com"
+// 	// password := apiKey
+// 	// recipient := "blessedmadukoma@gmail.com"
+// 	// subject := "Fancy subject!"
+// 	// body := "Hello from Mailgun Go!"
+
+// 	// // Compose the email
+// 	// emailBody := "From: " + sender + "\r\n" +
+// 	// 	"To: " + recipient + "\r\n" +
+// 	// 	"Subject: " + subject + "\r\n\r\n" +
+// 	// 	body
+
+// 	// // Set up authentication information
+// 	// auth := smtp.PlainAuth("", sender, password, "smtp.mailgun.org")
+
+// 	// // Connect to the server, authenticate, set the sender and recipient,
+// 	// // and send the email body.
+// 	// err := smtp.SendMail("smtp.mailgun.org:587", auth, sender, []string{recipient}, []byte(emailBody))
+// 	// if err != nil {
+// 	// 	log.Fatal(err)
+// 	// 	return false
+// 	// }
+
+// 	fmt.Println("Email sent successfully!")
+// 	return true
+// }
