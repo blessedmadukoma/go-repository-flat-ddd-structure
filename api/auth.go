2ca04ab793b193238295d054a3295680aa79dbee
blessedmadukoma <blessedmadukoma@gmail.com>
2024-04-30 12:51:07 +0100

    feat: change password through reset link

--- api/auth.go
+++ api/auth.go
@@ -66,15 +66,6 @@ func (c *Controller) Login(ctx *gin.Context) {
 		return
 	}
 
-	// // Resend registration token
-	// go func() {
-	// 	tasks.RegisterOtpTask(tasks.RegisterOtpInput{
-	// 		Email:     user.Email,
-	// 		FirstName: user.FirstName,
-	// 		OTP:       util.RandomOTP(),
-	// 	})
-	// }()
-
 	R.Data = user
 
 	ctx.JSON(messages.Response(0, R))
@@ -233,51 +224,3 @@ func (c Controller) PasswordResetChange(ctx *gin.Context) {
 
 	ctx.JSON(messages.Response(http.StatusOK, R))
 }
-
-// func (c Controller) PasswordResetChange(ctx *gin.Context) {
-// 	var R = ResponseFormat{}
-
-// 	// Validate input
-// 	var i validators.PasswordResetChangeInput
-// 	if err := ctx.ShouldBindJSON(&i); err != nil {
-// 		R.Error = append(R.Error, err.Error())
-// 		R.Message = messages.ValidationFailed
-// 		ctx.JSON(c.Response(http.StatusUnprocessableEntity, R))
-// 		return
-// 	}
-
-// 	a := models.Account{}
-
-// 	err := c.repo.DB.Model(models.Account{}).Joins("left join account_tokens on account_tokens.account_id = accounts.id").Preload("AccountToken").Where("account_tokens.token = ? AND accounts.email = ? AND account_tokens.type = ?", i.Token, i.Email, constants.AccountTokenTypePasswordResetKey).First(&a).Error
-// 	if err != nil {
-// 		R.Message = messages.InvalidToken
-// 		ctx.JSON(c.Response(http.StatusBadRequest, R))
-// 		return
-// 	}
-
-// 	hashedPassword, err := utils.Hash(i.NewPassword)
-// 	if err != nil {
-// 		log.Println("error in hashing new password: - ", err.Error())
-// 		R.Message = messages.InvalidToken
-// 		ctx.JSON(c.Response(http.StatusBadRequest, R))
-// 		return
-// 	}
-
-// 	c.repo.DB.Model(&models.Account{}).Where("id = ?", a.ID).Updates(
-// 		models.Account{
-// 			Password: string(hashedPassword),
-// 		})
-
-// 	// Notify user of password change
-// 	go func() {
-// 		tasks.PasswordChangeTask(tasks.PasswordChangeInput{
-// 			Email:     a.Email,
-// 			FirstName: a.FirstName,
-// 		})
-// 	}()
-
-// 	// Delete token
-// 	c.repo.DB.Delete(&a.AccountToken)
-
-// 	ctx.JSON(c.Response(http.StatusOK, R))
-// }
